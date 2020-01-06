cd4049e66134afda0017688ce9e0396815b6f6cc
jshen99 <shenoudajoseph7@gmail.com>
2020-01-06 02:40:39 -0500

    Gave the ability to delete someone off the waitlist by tapping their name

--- wheres_wass/screens/WaitlistScreen.component.js
+++ wheres_wass/screens/WaitlistScreen.component.js
@@ -3,6 +3,8 @@ import { StyleSheet,
          Text, 
          View,
          ScrollView,
+         FlatList,
+         SafeAreaView,
          Button,
          TouchableOpacity
          } from 'react-native';
@@ -30,42 +32,65 @@ class WaitlistScreen extends Component{
         this.waitRef = firebaseDatabase.ref('waitList/');
     }
 
-    //Setting header for navigation
-    static navigationOptions = ({ navigation }) =>  {
-		const headerObj = {
-			title: "Where's Wass",
-			
-			headerLeft: () => (
-				<FontAwesome.Button name="home" onPress =  {() => navigation.navigate("Home")} backgroundColor = 'black'/>
-			),
-        }
+    static navigationOptions = {
+        tabBarLabel : 'Wait List',
+        tabBarIcon : () => {
+            return <FontAwesome name="list" size={25} style = {{color:'black'}}/>
+        }     
 
-        return headerObj;
     }
+
+    Person({client}) {
+        
+        return (
+            <TouchableOpacity style={{flex : 1, justifyContent:'center', alignItems:'center', padding:20}} 
+            onPress = {() => firebaseDatabase.ref('waitList/'+client.id).remove().then( () => console.log('Successfully removed '+client.id) )}>
+                <Text style = {{fontSize: 25}}>{client.name}</Text>
+            </TouchableOpacity>
+        )
+    }
+    PersonStatic({client}) {
+        
+        return (
+            <View style={{flex : 1, justifyContent:'center', alignItems:'center', padding:20}}>
+                <Text style = {{fontSize: 25}}>{client.name}</Text>
+                <View style = {styles.separator}></View>
+            </View>
+        )
+    }
+
+
+
 
     //Listens and updates waitlist
     listenForWaitList(FBWaitRef){
         FBWaitRef.orderByKey().on('child_added', (snap) => {
             var identifier;
             console.log('Child added called for waitlistScreen')
-            if (snap.val().displayName){
-                identifier = snap.val().displayName;
+            if (snap.val().name){
+                identifier = snap.val().name;
             }
             else{
                 identifier = snap.val().email;
             }
             this.setState(prevState => ({
                 arrayOfWaitingClients : prevState.arrayOfWaitingClients.concat(
-                <View style={{flex : 1, justifyContent:'center', alignItems:'center'}}>
-                    <Text style = {{fontSize: 25}}>{identifier}</Text>
-                    <View style = {styles.separator}></View>
-                </View>
-                
-                
+                    {
+                        name : identifier,
+                        id : snap.key
+                    }
+                    
                 )
             }))
         })
+        
+        FBWaitRef.orderByKey().on('child_removed', (snap) => {
+            this.setState(prevState => ({
+                arrayOfWaitingClients : prevState.arrayOfWaitingClients.filter(client => client.id != snap.key)
+            }))
+        })
     }
+
     componentDidMount(){
         console.log('Called componentDidMount for waitlistScreen')
         this.listenForWaitList(this.waitRef)
@@ -75,15 +100,38 @@ class WaitlistScreen extends Component{
     }
 
     render(){
-        return(
-            <ScrollView horizontal={false} style={{flex:2}}>
-                {this.state.arrayOfWaitingClients}
-            </ScrollView>
-        )
+        if(this.props.removable){
+            return(
+                <SafeAreaView style={{flex:1}}>
+                    <FlatList
+                    data={this.state.arrayOfWaitingClients}
+                    renderItem = {({item}) => <this.Person client={item}/>}
+                    keyExtractor = {item => item.id}
+                    extraData={this.state.arrayOfWaitingClients}
+                    />
+                </SafeAreaView>
+            )
+        }
+        else{
+            return(
+                <SafeAreaView style={{flex:1}}>
+                    <FlatList
+                    data={this.state.arrayOfWaitingClients}
+                    renderItem = {({item}) => <this.PersonStatic client={item}/>}
+                    keyExtractor = {item => item.id}
+                    extraData={this.state.arrayOfWaitingClients}
+                    />
+                </SafeAreaView>
+            )
+        }
 
     }
 
     
+}
+
+WaitlistScreen.defaultProps = {
+    removable:true
 }
 
 const styles = StyleSheet.create({
@@ -91,7 +139,8 @@ const styles = StyleSheet.create({
 		width : '100%',
 		height : 5,
 		backgroundColor : 'orange'
-	    }
+    }
+    
 })
 
 export default WaitlistScreen;

a075dafa4800ecd1424c745f7bd2257e6bb8f4ec
jshen99 <shenoudajoseph7@gmail.com>
2020-01-07 22:03:43 -0500

    Added alerts to let users know they logged in successfully and put the textinputs for login with email on the Account screen

--- wheres_wass/screens/AccountScreen.component.js
+++ wheres_wass/screens/AccountScreen.component.js
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-import { View, Text, Button, TextInput, StyleSheet  } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet, Alert  } from 'react-native';
 import firebase,{facebookAppID} from '../config';
 import * as Facebook from 'expo-facebook';
 import { FontAwesome } from '@expo/vector-icons';
@@ -9,7 +9,9 @@ class Account extends Component{
     constructor(props){
 		super(props);
 		this.state = {
-			user : firebase.auth().currentUser
+			user : firebase.auth().currentUser,
+			email : '',
+			password : ''
 		}
 	}
 	//Setting the header for users to access nav drawer
@@ -24,20 +26,76 @@ class Account extends Component{
 		),}
 	}
 
-	async loginWithFacebook() {
-		await Facebook.initializeAsync(facebookAppID).then(console.log("Initialized Async"))
+	loginWithEmail(){
+		firebase.auth().signInWithEmailAndPassword(this.state.email,this.state.password).catch((error) => console.log("Failure in signing in with email and password"))
+		.then(
+			() => {
+				Alert.alert(
+					'Email Login',
+					'Login Successful!',
+					[
+					  {text: 'Go to Home', onPress: () => this.props.navigation.navigate('Home')},
+					],
+					{cancelable: false},
+				  )
+			}
+		)
 	
-		const { type, token } = await Facebook.logInWithReadPermissionsAsync(facebookAppID,
+    }
+
+	async loginWithFacebook() {
+		await Facebook.initializeAsync(facebookAppID).then(console.log("Initialized Async")).catch((error) => {
+			Alert.alert(
+				'Facebook Login',
+				'Login to facebook failed'+error.message,
+				[
+				  {text: 'Try Again', onPress: () => console.log('Ask me later pressed')},
+				  {
+					text: 'Cancel',
+					onPress: () => console.log('Cancel Pressed'),
+					style: 'cancel',
+				  },
+				],
+				{cancelable: false},
+			  );
+		})
+	
+		const { type, token, expires, permissions, declinedPermissions } = await Facebook.logInWithReadPermissionsAsync(facebookAppID,
 			{permissions: ['public_profile'] }
 		);
 	
 		if (type === 'success'){
+			//Prints out permissions
+			console.log('Granted permissions are  '+permissions)
+			console.log('Declined permissions are '+declinedPermissions)
+
 			//Build firebase credentials with the Facebook access token
 			const credential = firebase.auth.FacebookAuthProvider.credential(token);
 	
 			//Sign in with credential from the facebook user
-			firebase.auth().signInWithCredential(credential).then(this.props.navigation.push('Account')).catch((error) =>{
-			console.log("We have failed"+error.message)
+			firebase.auth().signInWithCredential(credential).then(
+				Alert.alert(
+					'Facebook Login',
+					'Login Successful!',
+					[
+					  {text: 'Go to Home', onPress: () => this.props.navigation.navigate('Home')},
+					],
+					{cancelable: false},
+				  )
+			).catch((error) =>{
+				Alert.alert(
+					'Facebook Login',
+					'Login to firebase failed'+error.message,
+					[
+					  {text: 'Try Again', onPress: () => console.log('try again pressed')},
+					  {
+						text: 'Cancel',
+						onPress: () => console.log('Cancel Pressed'),
+						style: 'cancel',
+					  },
+					],
+					{cancelable: false},
+				  );
 			});
 		}
 	}
@@ -70,16 +128,31 @@ class Account extends Component{
 		else{
 			return(
 			<View style = {accountStyles.container}>
-					<Button title = 'Login with Email' onPress= {() => this.props.navigation.navigate('Login') } />
+					<TextInput style = {style.textinput} autoCapitalize = 'none' placeholder = 'Enter Email' onChangeText = { (text) => this.setState({email : text}) } />	
+					<TextInput style = {style.textinput} autoCapitalize = 'none' placeholder = 'Enter Password' secureTextEntry = {true} onChangeText = {(text) => this.setState({password : text})} />
+					<Button title = 'Log in with email' onPress = {() => this.loginWithEmail()}/>
+
 					<Text> OR </Text>
 					<Button title = 'Login with Facebook' onPress = {() => this.loginWithFacebook()}/>
 					<Text>OR</Text>
 					<Button title = 'Create account with Email' onPress = {() => this.props.navigation.navigate('CreateAccount')} />
 
-				</View>
+			</View>
 			)
 		}
     }
 }
+
+
+const style = StyleSheet.create({
+    textinput : {
+	borderColor : 'orange',
+	borderWidth : 2,
+	padding : 10,
+	width : 300,
+	color : 'white'
+	
+    }
+})
 
 export default Account;

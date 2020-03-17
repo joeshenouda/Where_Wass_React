df3b6bfbfbab6137741c2aac07d141c44eec4377
jshen99 <shenoudajoseph7@gmail.com>
2020-03-16 20:58:44 -0400

    Updated notification sender function using expo-server sdk which chunks up the notifications

--- wheres_wass/functions/index.js
+++ wheres_wass/functions/index.js
@@ -2,11 +2,14 @@ const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
 const fetch = require("node-fetch");
+const { Expo } = require('expo-server-sdk')
 
 admin.initializeApp();
 
 
 exports.sendPushNotification = functions.database.ref('/Admin/news').onUpdate(async (change, context) => {
+
+    let expo = new Expo()
 
     //Checks if notify is set to true first
     notify = true
@@ -23,23 +26,41 @@ exports.sendPushNotification = functions.database.ref('/Admin/news').onUpdate(as
         const snapshot = await root.child('/expoTokens').once('value');
         snapshot.forEach(snap_2 => {
             var expoToken = snap_2.val().expoToken;
-            if (expoToken) {
+            if (Expo.isExpoPushToken(expoToken)) {
                 messages.push({
                     "to": expoToken,
                     "body": news
                 });
             }
+            else{
+                console.log('Push token ${expoToken} is not a valid expo token')
+            }
         });
         const messages_1 = await Promise.all(messages);
         console.log('About to send message');
-        fetch('https://exp.host/--/api/v2/push/send', {
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content_Type": "application/json"
-            },
-            body: JSON.stringify(messages_1)
-        }).then(res => console.log(res));
+
+
+        let chunks = expo.chunkPushNotifications(messages_1)
+
+        //Pushes the notification out in chunks so as not overload servers
+        (async () => {
+            for (let chunk of chunks) {
+              try {
+                let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+                console.log(ticketChunk);
+              } catch (error) {
+                console.error(error);
+              }
+            }
+          })();
+        // fetch('https://exp.host/--/api/v2/push/send', {
+        //     method: "POST",
+        //     headers: {
+        //         "Accept": "application/json",
+        //         "Content_Type": "application/json"
+        //     },
+        //     body: JSON.stringify(messages_1)
+        // }).then(res => console.log(res));
     }
 }
     

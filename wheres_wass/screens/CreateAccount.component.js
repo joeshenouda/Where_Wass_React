cf958af8e04b521906bcd1ea5a81d63b6bbf6596
jshen99 <shenoudajoseph7@gmail.com>
2020-06-12 16:17:26 -0400

    Add new email user to database and style update

--- wheres_wass/screens/CreateAccount.component.js
+++ wheres_wass/screens/CreateAccount.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Image, Button,TextInput,StyleSheet, Alert,Text, KeyboardAvoidingView } from 'react-native';
+import { View, Image, Button,TextInput,StyleSheet, Alert,Text, KeyboardAvoidingView, Platform } from 'react-native';
 //Because facebookAppID not the default export must be wrapped in curly
 import firebase from '../config';
 
@@ -25,6 +25,16 @@ class CreateAccountScreen extends Component {
                 ],
                 {cancelable: false},
             );
+        }
+        if(this.state.phoneNumber == '' || !/\d{10}/.test(this.state.phoneNumber)){
+            Alert.alert(
+                'Could not Create Account',
+                'Please enter your phone number',
+                [
+                    {text: 'OK', onPress: () => console.log('Ok Pressed')},
+                ],
+                {cancelable: false}
+            )
         }
         else{
             firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch((errors) => {
@@ -59,8 +69,10 @@ class CreateAccountScreen extends Component {
                             newUser.updateProfile(
                                 {
                                     displayName : this.state.name,
+                                    phoneNumber : this.state.phoneNumber
                                 }
                             ).then(() => {
+                                    firebase.database().ref('wadies').child(newUser.uid).update({phone: this.state.phoneNumber, email: this.state.email, username: this.state.name});
                                     this.props.navigation.navigate('Account')
                                 }
                             ).catch((error => console.log(error.message)))
@@ -80,14 +92,14 @@ class CreateAccountScreen extends Component {
     return(
         
             <KeyboardAvoidingView style={{flex:1}}
-                behavior = 'padding'>
+                behavior = {Platform.OS=='ios'?'padding':'height'}>
                 <View style={style.container} backgroundColor='black'>
                     <Text style={{fontSize:40, color:'white', alignSelf:'center'}}>Create Account</Text>
-                    <TextInput style={style.input}  placeholderTextColor='white' placeholder = 'Enter Full Name' onChangeText = { (text) => this.setState({name : text}) } />	
-                    <TextInput style={style.input}  placeholderTextColor='white' autoCapitalize = 'none' placeholder = 'Enter Phone number' keyboardType = 'number-pad' onChangeText = { (text) => this.setState({phoneNumber : text}) } />	
-                    <TextInput style={style.input}  placeholderTextColor='white' autoCapitalize = 'none' placeholder = 'Enter Email' onChangeText = { (text) => this.setState({email : text}) } />	
-                    <TextInput style={style.input}  placeholderTextColor='white' autoCapitalize = 'none' placeholder = 'Enter Password' secureTextEntry = {true} onChangeText = {(text) => this.setState({password : text})} />
-                    <TextInput style={style.input}  placeholderTextColor='white' autoCapitalize = 'none' placeholder = 'Re-Enter Password' secureTextEntry = {true} onChangeText = {(text) => this.setState({reenteredPS : text})} />
+                    <TextInput style={style.input}  placeholderTextColor='gray' placeholder = 'Enter Full Name' onChangeText = { (text) => this.setState({name : text}) } />	
+                    <TextInput style={style.input}  placeholderTextColor='gray' autoCapitalize = 'none' placeholder = 'Enter Phone number' keyboardType = 'number-pad' onChangeText = { (text) => this.setState({phoneNumber : text}) } />	
+                    <TextInput style={style.input}  placeholderTextColor='gray' autoCapitalize = 'none' placeholder = 'Enter Email' onChangeText = { (text) => this.setState({email : text}) } />	
+                    <TextInput style={style.input}  placeholderTextColor='gray' autoCapitalize = 'none' placeholder = 'Enter Password' secureTextEntry = {true} onChangeText = {(text) => this.setState({password : text})} />
+                    <TextInput style={style.input}  placeholderTextColor='gray' autoCapitalize = 'none' placeholder = 'Re-Enter Password' secureTextEntry = {true} onChangeText = {(text) => this.setState({reenteredPS : text})} />
                     <Button style={style.button} color= 'orange' title = 'Create Account' onPress = {() => this._onPressCreate()}/>
                 </View>
             </KeyboardAvoidingView>
@@ -103,8 +115,9 @@ const style = StyleSheet.create({
       },
       input: {
         fontSize: 20,
-        backgroundColor: 'black',
-        color: 'white',
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 10,
         borderBottomWidth : 1,
         borderBottomColor:'white'
       },
